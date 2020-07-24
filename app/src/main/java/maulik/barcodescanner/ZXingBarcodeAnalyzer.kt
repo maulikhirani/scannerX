6d3934a74f154c4b6c620bc43e05ddd799ea96c9
Maulik Hirani <maulikhirani@gmail.com>
2020-07-24 23:39:43 +0530

    Resolved issue of incorrect exception handled for zxing scanning.

--- app/src/main/java/maulik/barcodescanner/ZXingBarcodeAnalyzer.kt
+++ app/src/main/java/maulik/barcodescanner/ZXingBarcodeAnalyzer.kt
@@ -1,27 +1,39 @@
 package maulik.barcodescanner
 
-import android.content.res.Resources.NotFoundException
 import android.graphics.ImageFormat
 import android.util.Log
 import androidx.camera.core.ImageAnalysis
 import androidx.camera.core.ImageProxy
 import com.google.zxing.BinaryBitmap
 import com.google.zxing.MultiFormatReader
+import com.google.zxing.NotFoundException
 import com.google.zxing.PlanarYUVLuminanceSource
 import com.google.zxing.common.HybridBinarizer
 import java.nio.ByteBuffer
+import java.util.concurrent.atomic.AtomicBoolean
 
 
 class ZXingBarcodeAnalyzer : ImageAnalysis.Analyzer {
 
     private var multiFormatReader: MultiFormatReader = MultiFormatReader()
+    private var isScanning = AtomicBoolean(false)
 
     override fun analyze(image: ImageProxy) {
+
+        if (isScanning.get()) {
+            image.close()
+            return
+        }
+
+        isScanning.set(true)
+
         if ((image.format == ImageFormat.YUV_420_888 || image.format == ImageFormat.YUV_422_888
-                    || image.format == ImageFormat.YUV_444_888) && image.planes.size == 3) {
-            val buffer: ByteBuffer = image.planes.get(0).buffer
+                    || image.format == ImageFormat.YUV_444_888) && image.planes.size == 3
+        ) {
+            val buffer: ByteBuffer = image.planes[0].buffer
             val bytes = ByteArray(buffer.remaining())
             buffer.get(bytes)
+
             val planarYUVLuminanceSource = PlanarYUVLuminanceSource(
                 bytes,
                 image.width,
@@ -42,6 +54,8 @@ class ZXingBarcodeAnalyzer : ImageAnalysis.Analyzer {
                 multiFormatReader.reset()
                 image.close()
             }
+
+            isScanning.set(false)
         }
     }
 }
